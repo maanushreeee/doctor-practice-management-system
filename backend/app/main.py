70f896c12d254fb8cbb03fe08cb10b691b42f65a
maanushreeee <maanushreemisal@gmail.com>
2026-03-22 15:33:54 +0530

    cors bug fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://doctor-practice-management-system-ad30.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
